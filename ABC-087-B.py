732c6c25fb5a880d4202ea7e6f906a90154d091c
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-04-09 20:34:34 +0900

    .

---
+++ ABC-087-B.py
@@ -0,0 +1,8 @@
+A,B,C,X = int(input()),int(input()),int(input()),int(input())
+n = 0
+for i in range(0,A+1):
+    for j in range(0,B+1):
+        for k in range(0,C+1):
+            if 500*i+100*j+50*k == X:
+                n=n+1
+print(n)
