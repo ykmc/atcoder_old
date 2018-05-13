5b7b6e7afece86e6a1f10c8fe1f596a462b55176
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-05-13 19:46:45 +0900

    .

--- ABC-097-D.py
+++ ABC-097-D.py
@@ -1,42 +1,36 @@
 N,M = map(int,input().split())
 P = list(map(int,input().split()))
-X,Y = [],[]
-for i in range(M):
+
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n+1)]
+        self.rank = [0] * (n+1)
+    def find(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if self.rank[x] < self.rank[y]:
+            self.par[x] = y
+        else:
+            self.par[y] = x
+            if self.rank[x] == self.rank[y]:
+                self.rank[x] += 1
+    def check(self, x, y):
+        return self.find(x) == self.find(y)
+
+uf = UnionFind(N)
+
+for _ in range(M):
     x,y = map(int,input().split())
-    X.append(x-1)
-    Y.append(y-1)
-T = [-1]*N
-group = 0
-for i in range(M):
-    if T[X[i]] == -1 and T[Y[i]] == -1:
-        T[X[i]] = group
-        T[Y[i]] = group
-        group += 1
-    elif T[X[i]] != -1 and T[Y[i]] != -1:
-        minG = min(T[X[i]],T[Y[i]])
-        maxG = max(T[X[i]],T[Y[i]])
-        for i in range(N):
-            if T[i] == maxG:
-                T[i] = minG
-    else:
-        if T[X[i]] == -1 and T[Y[i]] != -1:
-            T[X[i]] = T[Y[i]]
-        if T[Y[i]] == -1 and T[X[i]] != -1:
-            T[Y[i]] = T[X[i]]
-uniqueT = list(set(T))
-uniqueT.sort()
-ansT = [[] for _ in range(len(uniqueT))]
-ansP = [[] for _ in range(len(uniqueT))]
-for i in range(len(uniqueT)):
-    if i != -1:
-        for j in range(N):
-            if T[j] == uniqueT[i]:
-                ansT[i].append(j)
-                ansP[i].append(P[j]-1)
+    uf.unite(x,y)
+
 Ans = 0
-for i in range(len(uniqueT)):
-    Ans += len(list(set(ansT[i]) & set(ansP[i])))
-for i in range(N):
-    if T[i] == -1 and P[i]-1 == i:
-        Ans += 1
+for i,p in enumerate(P,1):
+    Ans += 1 if uf.check(i,p) else 0
+
 print(Ans)
