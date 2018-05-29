e34429a4feeffbb7e7cd0732240da61739d1abf5
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-05-29 21:48:48 +0900

    .

--- ABC-038-C.py
+++ ABC-038-C.py
@@ -1,11 +1,17 @@
 N = int(input())
 A = list(map(int,input().split()))
-Ans,t,prev = 0,0,0
-for i in range(N):
-    if A[i] > prev:
-        t += 1
+
+Ans,prev,Li,Ri = 0,1,0,0
+# 右端がN未満なら順次伸ばす
+while Ri < N:
+    # 条件を満たすなら答えを更新
+    if A[Ri] > prev:
+        Ans += Ri-Li+1
+    # 条件を満たさないなら左端を調整
     else:
-        t = 1
-    prev = A[i]
-    Ans += t
+        # 条件を満たすまで左端を縮める
+        Li = Ri
+        Ans += 1
+    prev = A[Ri]
+    Ri += 1
 print(Ans)
