38060b72cf780bbae9186363d5931cb10ec5b1ec
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-06-24 00:37:47 +0900

    .

--- ABC-070-D.py
+++ ABC-070-D.py
@@ -1,11 +1,15 @@
+import sys
+sys.setrecursionlimit(1000000)
+
 N = int(input())
 X = [[] for _ in range(N)]
+
 for i in range(N-1):
     a,b,c = map(int,input().split())
     X[a-1].append((b-1,c))
     X[b-1].append((a-1,c))
 
-cost = [-1]*(N+2)
+cost = [-1]*N
 def calc(i,c):
     # 指定の頂点までのコストを代入
     cost[i] = c
@@ -21,5 +25,5 @@ calc(K-1,0)
 
 for i in range(Q):
     x,y = map(int,input().split())
-    #print(cost[x-1]+cost[y-1])
+    print(cost[x-1]+cost[y-1])
 
