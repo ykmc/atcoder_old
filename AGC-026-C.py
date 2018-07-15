2c8496857ba4580527ca5fc7a8707531c7548332
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-07-15 16:11:34 +0900

    .

--- AGC-026-C.py
+++ AGC-026-C.py
@@ -1,3 +1,40 @@
 N = int(input())
 S = input()
 
+S1,S2 = S[:N],S[N:][::-1]
+
+from itertools import product
+B = list(product([0,1], repeat=N))
+
+D1 = {}
+Ans = 0
+
+for bb in B:
+    red,blue = "",""
+    cnt = 0
+    for i,b in enumerate(bb):
+        if b == 0:
+            red += S1[i]
+            cnt += 1
+        else:
+            blue += S1[i]
+    key = (cnt,red+blue[::-1])
+    if key not in D1.keys():
+        D1[key] = 1
+    else:
+        D1[key] += 1
+
+for bb in B:
+    red,blue = "",""
+    cnt = 0
+    for i,b in enumerate(bb):
+        if b == 1:
+            red += S2[i]
+            cnt += 1
+        else:
+            blue += S2[i]
+    key = (cnt,red+blue[::-1])
+    if key in D1.keys():
+        Ans += D1[key]
+
+print(Ans)
