c3741c9cc8d958625c8cca3c3dfa09fcfc5d3425
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-07-28 23:19:45 +0900

    .

---
+++ Rust/ABC042-070/ABC-044-A.rs
@@ -0,0 +1,47 @@
+macro_rules! input {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            input!($t)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main(){
+    let n = input!(usize);
+    let k = input!(usize);
+    let x = input!(usize);
+    let y = input!(usize);
+    let ans;
+    if n<=k{
+        ans = x*n;
+    }else{
+        ans = x*k + y*(n-k);
+    }
+    println!("{}", ans)
+}
