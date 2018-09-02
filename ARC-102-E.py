aaf776a26ac35d8287cea9acf74367e290aff568
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-09-02 11:55:35 +0900

    .

--- ARC-102-E.py
+++ ARC-102-E.py
@@ -1,5 +1,6 @@
 K,N = map(int,input().split())
 
+
 MOD = 998244353
 
 fact = [1]*4000
@@ -13,10 +14,16 @@ for i in range(1,4000):
     factinv[i] = pow(fact[i], MOD-2, MOD)
 
 def ncr(n,r):
+    if n < 0 or r < 0 or n-r < 0:
+        return 0
     return (fact[n] * factinv[r] * factinv[n-r]) % MOD
 
-ans = []
-for i in range(2,K+2):
+def nhr(n,r):
+    if n == 0 and r == 0:
+        return 1
+    return ncr(n+r-1, r)
+
+for i in range(2,2*K+1):
     t1,t2 = 0,0
     Ans = 0
     # p : 足して i になる整数の組の数
@@ -28,30 +35,23 @@ for i in range(2,K+2):
         np -= 1
         for nq in range(np+1):
             # 同じ数の組の数字を全く含まない場合
-            t1 = ncr(np,nq)*pow(2,nq,MOD)
+            t1 = ncr(np,nq)*pow(2, nq, MOD)
             t1 %= MOD
-            nr = K - np*2 - 1
-            t2 = ncr(N+nr-1,N-nq) if N+nr-1>=N-nq else 0
+            nr = K-np*2-1
+            t2 = nhr(nr+nq,N-nq)
             t2 %= MOD
-            Ans += t1*t2
+            Ans = (Ans + t1*t2) % MOD
             # 同じ数の組の数字を1つ含む場合
-            t2 = ncr(N-1+nr-1,N-1-nq) if N-1+nr-1>=N-1-nq else 0
+            t2 = nhr(nr+nq,N-nq-1)
             t2 %= MOD
             Ans = (Ans + t1*t2) % MOD
     else:
         # 奇数の場合 : p組全て互いに異なる数
         for nq in range(np+1):
-            t1 = ncr(np,nq)*pow(2,nq,MOD)
+            t1 = ncr(np,nq)*pow(2, nq, MOD)
             t1 %= MOD
-            nr = K - np*2
-            t2 = ncr(N+nr-1,N-nq) if N-1+nr-1>=N-1-nq else 0
+            nr = K-np*2
+            t2 = nhr(nr+nq,N-nq)
             t2 %= MOD
             Ans = (Ans + t1*t2) % MOD
-    ans.append(Ans%MOD)
-
-for a in ans:
-    print(a)
-ans.pop(len(ans)-1)
-ans = ans[::-1]
-for a in ans:
-    print(a)
+    print(Ans%MOD)
