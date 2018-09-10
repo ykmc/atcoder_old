0f464990ff8dc15b41e10848e588e97365c2b926
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-09-10 20:49:14 +0900

    .

---
+++ 2018/0805_ABC104/B.rs
@@ -0,0 +1,78 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+ 
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+ 
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+ 
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+ 
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+ 
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+ 
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+ 
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+ 
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+// -----------------------------------------------------------------------------
+// main
+// -----------------------------------------------------------------------------
+fn main(){
+    input!{
+        s: chars
+    }
+    let mut ans = "WA";
+    if s[0]=='A' 
+      && s[2..s.len()-1].iter().filter(|&&c| c=='C').count() == 1
+      && s.iter().filter(|c| c.is_uppercase()).count() == 2
+    {
+        ans = "AC";
+    }
+    println!("{}", ans);
+}
+
