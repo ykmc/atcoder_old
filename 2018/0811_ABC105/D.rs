81ba891bfd2b1a53601da3d6a6306af4139ed384
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-09-13 22:25:17 +0900

    .

---
+++ 2018/0811_ABC105/D.rs
@@ -0,0 +1,86 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+ 
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+ 
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+ 
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+ 
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+ 
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+ 
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+ 
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+ 
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+// -----------------------------------------------------------------------------
+// main
+// -----------------------------------------------------------------------------
+use std::collections::HashMap;
+fn main(){
+    input!{
+        n: usize,
+        m: usize,
+        a: [usize; n]
+    }
+    let mut ans:usize = 0;
+    let mut sa = vec![0;n+1];
+    for i in 0..n{
+        sa[i+1] = (sa[i]+a[i])%m;
+    }
+    let mut map = HashMap::new();
+    for x in sa{
+        *map.entry(x).or_insert(0) += 1;
+    }
+    for (_k,v) in map{
+        ans += (v*(v-1))/2;
+    }
+    println!("{}", ans);
+}
+
