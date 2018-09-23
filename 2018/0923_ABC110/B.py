db678ed742aecc83431690352b1eda7828a5fcdd
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-09-23 22:01:50 +0900

    .

---
+++ 2018/0923_ABC110/B.py
@@ -0,0 +1,12 @@
+N,M,X,Y = map(int,input().split())
+x = list(map(int,input().split()))
+y = list(map(int,input().split()))
+
+Ans = "War"
+maxX = max(x)
+minY = min(y)
+for i in range(maxX,minY+1):
+    if X < i <= Y:
+        Ans = "No War"
+
+print(Ans)
