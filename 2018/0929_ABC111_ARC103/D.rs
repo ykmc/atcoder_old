a7ffa568ab9f963bedbd8851b7585ec1ae0c36f7
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2018-09-30 20:45:22 +0900

    .

---
+++ 2018/0929_ABC111_ARC103/D.rs
@@ -0,0 +1,32 @@
+#![allow(non_snake_case, unused_macros, unused_imports)]
+
+// Input
+macro_rules! input { (source = $s:expr, $($r:tt)*) => { let mut iter = $s.split_whitespace(); let mut next = || { iter.next().unwrap() }; input_inner!{next, $($r)*} }; ($($r:tt)*) => { let stdin = std::io::stdin(); let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock())); let mut next = move || -> String{ bytes.by_ref().map(|r|r.unwrap() as char).skip_while(|c|c.is_whitespace()).take_while(|c|!c.is_whitespace()).collect() }; input_inner!{next, $($r)*} }; }
+macro_rules! input_inner { ($next:expr) => {}; ($next:expr, ) => {}; ($next:expr, $var:ident : $t:tt $($r:tt)*) => { let $var = read_value!($next, $t); input_inner!{$next $($r)*} };}
+macro_rules! read_value { ($next:expr, ( $($t:tt),* )) => { ( $(read_value!($next, $t)),* ) }; ($next:expr, [ $t:tt ; $len:expr ]) => { (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>() }; ($next:expr, chars) => { read_value!($next, String).chars().collect::<Vec<char>>() }; ($next:expr, bytes) => { read_value!($next, String).into_bytes() }; ($next:expr, usize1) => { read_value!($next, usize) - 1 }; ($next:expr, $t:ty) => { $next().parse::<$t>().expect("Parse error") }; }
+
+// Module
+use std::cmp::{min,max};
+use std::collections::{HashSet,BTreeMap};
+
+// Main
+fn main(){
+    input!{
+        n: usize,
+        xy: [(i64,i64); n],
+    }
+    // パリティのチェック
+    let mut even = 0;
+    let mut odd = 0;
+    for i in 0..n{
+        if (xy[i].0 + xy[i].1)%2==0{
+            even += 1;
+        }else{
+            odd += 1;
+        }
+    }
+    if even>0 && odd>0 {
+        println!("{}", -1);
+        return;
+    }
+}
