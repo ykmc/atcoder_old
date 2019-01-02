aaaeba7205dca69f941e642b69c4a86f52579ab7
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2019-01-02 20:30:42 +0900

    21/2019

---
+++ 2016/C_20190102.py
@@ -0,0 +1,20 @@
+N,A = map(int,input().split())
+X = [0]+list(map(int,input().split()))
+
+# dp[n][k][s] : n枚目までからk枚選んで合計をsにする場合の数
+dp = [[[0 for _ in range(2501)] for _ in range(N+1)] for _ in range(N+1)]
+dp[0][0][0] = 1
+
+for n in range(1,N+1):
+    for k in range(n+1):
+        for s in range(50*k+1):
+            if n>=1 and s<X[n]:
+                dp[n][k][s] = dp[n-1][k][s]
+            elif n>=1 and k>=1 and s>=X[n]:
+                dp[n][k][s] = dp[n-1][k][s] + dp[n-1][k-1][s-X[n]]
+
+Ans = 0
+for i in range(1,N+1):
+    Ans += dp[N][i][i*A]
+
+print(Ans)
