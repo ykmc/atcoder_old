716fc6e34af543abc766c3c4c2e00f5ff8936015
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2019-01-12 18:30:28 +0900

    137/2019

---
+++ 2017/0422_ABC059_ARC072/C_20190112.rs
@@ -0,0 +1,54 @@
+#![allow(non_snake_case, unused_imports)]
+
+// Input
+macro_rules! input { (source = $s:expr, $($r:tt)*) => { let mut iter = $s.split_whitespace(); let mut next = || { iter.next().unwrap() }; input_inner!{next, $($r)*} }; ($($r:tt)*) => { let stdin = std::io::stdin(); let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock())); let mut next = move || -> String{ bytes.by_ref().map(|r|r.unwrap() as char).skip_while(|c|c.is_whitespace()).take_while(|c|!c.is_whitespace()).collect() }; input_inner!{next, $($r)*} }; }
+macro_rules! input_inner { ($next:expr) => {}; ($next:expr, ) => {}; ($next:expr, $var:ident : $t:tt $($r:tt)*) => { let $var = read_value!($next, $t); input_inner!{$next $($r)*} };}
+macro_rules! read_value { ($next:expr, ( $($t:tt),* )) => { ( $(read_value!($next, $t)),* ) }; ($next:expr, [ $t:tt ; $len:expr ]) => { (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>() }; ($next:expr, chars) => { read_value!($next, String).chars().collect::<Vec<char>>() }; ($next:expr, bytes) => { read_value!($next, String).into_bytes() }; ($next:expr, usize1) => { read_value!($next, usize) - 1 }; ($next:expr, $t:ty) => { $next().parse::<$t>().expect("Parse error") }; }
+
+// Module
+use std::cmp::{min,max};
+use std::collections::{HashSet,HashMap,BTreeMap,VecDeque};
+
+// Functions
+
+// Main
+fn main(){
+    input!{
+        N: usize,
+        A: [i64; N],
+    }
+    let mut ans1 = 0_i64;
+    let mut ans2 = 0_i64;
+    let mut res = 0_i64;
+    for i in 0..N{
+        res += A[i];
+        if i%2==0{
+            if res <= 0{
+                ans1 -= res-1;
+                res = 1;
+            }
+        }else{
+            if res >= 0{
+                ans1 += res+1;
+                res = -1;
+            }
+        }
+    }
+
+    res = 0;
+    for i in 0..N{
+        res += A[i];
+        if i%2==1{
+            if res <= 0{
+                ans2 -= res-1;
+                res = 1;
+            }
+        }else{
+            if res >= 0{
+                ans2 += res+1;
+                res = -1;
+            }
+        }
+    }
+    println!("{}",min(ans1,ans2));
+}
