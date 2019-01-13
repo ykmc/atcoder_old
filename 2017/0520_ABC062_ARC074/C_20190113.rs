b9fd646bc5d7cdd259085b94436488e11a6fd059
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2019-01-13 20:58:45 +0900

    159/2019

---
+++ 2017/0520_ABC062_ARC074/C_20190113.rs
@@ -0,0 +1,48 @@
+#![allow(non_snake_case, unused_imports)]
+
+// Input
+macro_rules! input { (source = $s:expr, $($r:tt)*) => { let mut iter = $s.split_whitespace(); let mut next = || { iter.next().unwrap() }; input_inner!{next, $($r)*} }; ($($r:tt)*) => { let stdin = std::io::stdin(); let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock())); let mut next = move || -> String{ bytes.by_ref().map(|r|r.unwrap() as char).skip_while(|c|c.is_whitespace()).take_while(|c|!c.is_whitespace()).collect() }; input_inner!{next, $($r)*} }; }
+macro_rules! input_inner { ($next:expr) => {}; ($next:expr, ) => {}; ($next:expr, $var:ident : $t:tt $($r:tt)*) => { let $var = read_value!($next, $t); input_inner!{$next $($r)*} };}
+macro_rules! read_value { ($next:expr, ( $($t:tt),* )) => { ( $(read_value!($next, $t)),* ) }; ($next:expr, [ $t:tt ; $len:expr ]) => { (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>() }; ($next:expr, chars) => { read_value!($next, String).chars().collect::<Vec<char>>() }; ($next:expr, bytes) => { read_value!($next, String).into_bytes() }; ($next:expr, usize1) => { read_value!($next, usize) - 1 }; ($next:expr, $t:ty) => { $next().parse::<$t>().expect("Parse error") }; }
+
+// Module
+use std::cmp::{min,max};
+use std::collections::{HashSet,HashMap,BTreeMap,VecDeque};
+
+// Functions
+
+// Main
+fn main(){
+    input!{
+        H: usize,
+        W: usize,
+    }
+    let mut ans = 999999999;
+    let (mut s1, mut s2, mut s3) = (1,1,1);
+    // 高さで分割するのを全探索、一方を更に高さor幅で分割
+    for h in 1..H{
+        s1 = h*W;
+        // 高さで分割
+        s2 = ((H-h)/2)*W;
+        s3 = (H-h-(H-h)/2)*W;
+        ans = min(ans, max(s1,max(s2,s3))-min(s1,min(s2,s3)));
+        // 幅で分割
+        s2 = (H-h)*(W/2);
+        s3 = (H-h)*(W-W/2);
+        ans = min(ans, max(s1,max(s2,s3))-min(s1,min(s2,s3)));
+    }
+    // 高さと幅を入れ替えて再実行
+    let (H,W) = (W,H);
+    for h in 1..H{
+        s1 = h*W;
+        // 高さで分割
+        s2 = ((H-h)/2)*W;
+        s3 = (H-h-(H-h)/2)*W;
+        ans = min(ans, max(s1,max(s2,s3))-min(s1,min(s2,s3)));
+        // 幅で分割
+        s2 = (H-h)*(W/2);
+        s3 = (H-h)*(W-W/2);
+        ans = min(ans, max(s1,max(s2,s3))-min(s1,min(s2,s3)));
+    }
+    println!("{}",ans);
+}
