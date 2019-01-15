44f0f20afb9f17c4daf199b52e261c8abf601909
dynamis724 <34961813+dynamis724@users.noreply.github.com>
2019-01-16 00:15:49 +0900

    169/2019

---
+++ 2017/0610_ABC064/D_20190116.rs
@@ -0,0 +1,49 @@
+#![allow(non_snake_case, unused_imports)]
+
+// Input
+macro_rules! input { (source = $s:expr, $($r:tt)*) => { let mut iter = $s.split_whitespace(); let mut next = || { iter.next().unwrap() }; input_inner!{next, $($r)*} }; ($($r:tt)*) => { let stdin = std::io::stdin(); let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock())); let mut next = move || -> String{ bytes.by_ref().map(|r|r.unwrap() as char).skip_while(|c|c.is_whitespace()).take_while(|c|!c.is_whitespace()).collect() }; input_inner!{next, $($r)*} }; }
+macro_rules! input_inner { ($next:expr) => {}; ($next:expr, ) => {}; ($next:expr, $var:ident : $t:tt $($r:tt)*) => { let $var = read_value!($next, $t); input_inner!{$next $($r)*} };}
+macro_rules! read_value { ($next:expr, ( $($t:tt),* )) => { ( $(read_value!($next, $t)),* ) }; ($next:expr, [ $t:tt ; $len:expr ]) => { (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>() }; ($next:expr, chars) => { read_value!($next, String).chars().collect::<Vec<char>>() }; ($next:expr, bytes) => { read_value!($next, String).into_bytes() }; ($next:expr, usize1) => { read_value!($next, usize) - 1 }; ($next:expr, $t:ty) => { $next().parse::<$t>().expect("Parse error") }; }
+
+// Module
+use std::cmp::{min,max};
+use std::collections::{HashSet,HashMap,BTreeMap,VecDeque};
+
+// Functions
+
+// Main
+fn main(){
+    input!{
+        N: usize,
+        S: chars,
+    }
+    // "("*L , S, ")"*R
+    let (mut L, mut R) = (0,0);
+    // "(" が余っている数
+    let mut rem = 0;
+
+    for i in 0..N{
+        // ')'
+        if S[i]==')'{
+            if rem==0{
+                L += 1;
+            }else{
+                R -= 1;
+                rem -= 1;
+            }
+        // '('
+        }else{
+            R += 1;
+            rem += 1;
+        }
+    }
+    for _ in 0..L{
+        print!("(");
+    }
+    for s in S{
+        print!("{}", s);
+    }
+    for _ in 0..R{
+        print!(")");
+    }
+}
